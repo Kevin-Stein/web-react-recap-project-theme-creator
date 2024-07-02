c5efd36bd6334e6f210b9d55a212e4eaf07e1a33
Kevin <kevinstein@gmx.net>
2024-07-02 14:24:36 +0200

    add color form function

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
 import ColorForm from "./Components/ColorForm";
+import { useState } from "react";
 
 function App() {
+  const [colorArray, setColorArray] = useState(initialColors);
+
+  function addColor(newColor) {
+    setColorArray([{ id: crypto.randomUUID(), ...newColor }, ...colorArray]);
+  }
   return (
     <>
       <h1>Theme Creator</h1>
-      <ColorForm />
-      {initialColors.map((color) => {
+      <ColorForm colorSubmit={addColor} />
+      {colorArray.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
